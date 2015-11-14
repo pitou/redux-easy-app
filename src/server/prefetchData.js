2eb95ab4a019d948bb09645987b4a4028902a601
ljadmin <admin@poetcyb.org>
2015-11-14 19:01:21 +0100

    prefetchData: fix regex matching path instead of decodedPath

--- src/server/prefetchData.js
+++ src/server/prefetchData.js
@@ -4,13 +4,13 @@ export default function(routesFetchersMap, path, store) {
     console.log("Pre-fetcher --> Decoded path: " + decodedPath);
 
     for (let i = 0; i < routesFetchersMap.length; i++) {
-        const m = routesFetchersMap[i].regex.exec(path);
+        const m = routesFetchersMap[i].regex.exec(decodedPath);
         if (m !== null) {
             console.log(`Pre-fetcher --> Matched ${routesFetchersMap[i].name}`);
             return store.dispatch(routesFetchersMap[i].func(m));
         }
     }
 
-    console.log(`Pre-fetcher --> No route matched`);
+    console.log(`Pre-fetcher --> No route matched, nothing to prefetch`);
     return Promise.resolve(true);
 }
