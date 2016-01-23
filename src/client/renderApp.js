ef59d310aef0ccbb507385e422f2aa4e07818d1d
pitou <me@poetcyb.org>
2016-01-23 21:04:16 +0100

    createApp after setting __CLIENT__ to true

--- src/client/renderApp.js
+++ src/client/renderApp.js
@@ -3,15 +3,15 @@ import React from 'react';  // eslint-disable-line no-unused-vars
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
-import createStore from '../createStore';
-import createApp from '../createApp';
-
 export default function(options) {
 
     const { reducers, routes, appRootElement } = options;
 
     window.__CLIENT__ = true;
     window.__SERVER__ = false;
+
+    const createStore = require('../createStore');
+    const createApp = require('../createApp');
 
     const state = window.__initialState;
     const store = createStore(reducers, state);
