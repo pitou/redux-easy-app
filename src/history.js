c1920077118b7cbd1a036c4f248794397a05d9a0
pitou <me@poetcyb.org>
2016-01-23 18:53:19 +0100

    fixing migration to redux-simple-router

--- src/history.js
+++ src/history.js
@@ -2,4 +2,4 @@ import createBrowserHistory from 'history/lib/createBrowserHistory';
 
 const isBrowser = typeof window !== 'undefined' && window.__CLIENT_;
 
-export default history = isBrowser ? createBrowserHistory() : { listen: () => {} };
+export default isBrowser ? createBrowserHistory() : { listen: () => {} };
