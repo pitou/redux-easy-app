44930f893ed9d4b14d4e86ff5f0700809876b0d2
pitou <me@poetcyb.org>
2016-01-24 13:53:51 +0100

    avoid calling mapDispatchToProps (thanks to @gaeron)

--- src/createContainer.js
+++ src/createContainer.js
@@ -1,11 +1,6 @@
-import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
 
 export default function(component, actions, mapStateToProps) {
 
-    function mapDispatchToProps(dispatch) {
-        return bindActionCreators(actions, dispatch);
-    }
-
-    return connect(mapStateToProps, mapDispatchToProps)(component);
+    return connect(mapStateToProps, actions)(component);
 }
