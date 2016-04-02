ce6df4deb2aabb87da94ec5e97a466fb701b9827
pitou <me@poetcyb.org>
2016-04-02 17:33:23 +0200

    big dependencies update

--- src/server/renderApp.js
+++ src/server/renderApp.js
@@ -1,3 +1,4 @@
+import 'babel-polyfill';
 import React from 'react';
 import { Provider } from 'react-redux';
 import ReactDOMServer from 'react-dom/server';
--- src/server/startApp.js
+++ src/server/startApp.js
@@ -1,4 +1,4 @@
-import 'babel/polyfill';
+import 'babel-polyfill';
 import bootstrap from './bootstrap';
 
 export default function(expressApp, options) {
