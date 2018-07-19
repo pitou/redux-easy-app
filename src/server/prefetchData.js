27e31526a8c87039d42a82a802a34560e8e3add4
Vito Latrofa <vito.lat@gmail.com>
2018-07-19 15:07:00 +0200

    make multiple dispatch if multiple routes matched

--- src/server/prefetchData.js
+++ src/server/prefetchData.js
@@ -3,12 +3,19 @@ export default function(routesFetchersMap, path, store) {
     const decodedPath = decodeURI(path);
     console.log("Pre-fetcher --> Decoded path: " + decodedPath);
 
+    const promises = [];
+
     for (let i = 0; i < routesFetchersMap.length; i++) {
-        const m = routesFetchersMap[i].regex.exec(decodedPath);
+        const { regex, name, func } = routesFetchersMap[i];
+
+        const m = regex.exec(decodedPath);
         if (m !== null) {
-            console.log(`Pre-fetcher --> Matched ${routesFetchersMap[i].name}`);
-            return store.dispatch(routesFetchersMap[i].func(m));
+            console.log(`Pre-fetcher --> Matched ${name}`);
+            promises.push(store.dispatch(func(m)));
         }
+    }
+    if (promises.length > 0) {
+      return Promise.all(promises);
     }
 
     console.log(`Pre-fetcher --> No route matched, nothing to prefetch`);
