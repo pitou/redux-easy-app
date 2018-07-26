fe15af9fc80bb474a73fbd4628c479b700e5662f
Vito Latrofa <vito.lat@gmail.com>
2018-07-26 17:18:51 +0200

    onError callback; do not expose error messages

--- src/server/bootstrap.js
+++ src/server/bootstrap.js
@@ -8,11 +8,12 @@ import prefetchData from './prefetchData';
 export default (req, res, options) => {
 
     const { routes, reducers, initialState, routesFetchersMap, ignoredPathsRegex } = options;
+    const onError = options.onError || (() => {});
 
     match({ routes, location: req.url }, (error, redirectLocation, renderProps) => {
         if (error) {
-          console.log(error);
-          return res.status(500).end('Internal server error');
+          onError(error);
+          return res.status(200).end('');
         }
         if (redirectLocation) {
           return res.status(301).redirect(redirectLocation.pathname);
@@ -26,6 +27,9 @@ export default (req, res, options) => {
         prefetchData(routesFetchersMap, renderProps.location.pathname, req.query, ignoredPathsRegex, store)
             .then(() => renderApp(renderProps, store, options))
             .then(html => res.send(html))
-            .catch(err => res.send(`Error: ${err.stack}`));
+            .catch(err => {
+              onError(err);
+              return res.status(200).end('');
+            });
     });
 }
